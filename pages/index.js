1c4c3a1cb8ae3be06af89adb5a4b6e7a56845b2d
Code Done Right <sciencedoneright5@gmail.com>
2022-03-29 21:41:13 +0530

    Made some small changes like adding the favicon.ico

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,10 @@ import Head from "next/head";
 
 export default function HomePage() {
   return (
-    <div>
+    <div className="bg-slate-700">
       <Head>
         <title>Sciencium</title>
+        <link rel="shortcut icon" href="../public/favicon.ico" />
       </Head>
 
       <p className="font-bold text-xl antialiased">Hello World! 🚀</p>
